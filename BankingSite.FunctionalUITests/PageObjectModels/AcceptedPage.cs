af294d4675f8ab0bd8b651d216a8ae810c9f0fff
Алексей <poltavaaa@gmail.com>
2017-06-02 17:13:10 +0300

    Changed user start type

--- BankingSite.FunctionalUITests/PageObjectModels/AcceptedPage.cs
+++ BankingSite.FunctionalUITests/PageObjectModels/AcceptedPage.cs
@@ -5,6 +5,8 @@ namespace BankingSite.FunctionalUITests.PageObjectModels
 {
     class AcceptedPage : Page
     {
+
+
         public string AcceptedMessage
         {
             get { return Find.Element(By.Id("acceptMessage")).Text; }
